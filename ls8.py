10f3ee0eaba4d6d85b1b23666f7c281fc077b5f8
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-05-25 21:26:17 -0700

    Added new cpu and ls8 from classwork done this weekend

---
+++ ls8.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Main."""
+
+import sys
+from cpu import *
+
+cpu = CPU()
+
+if len(sys.argv) != 2:
+    print("Improper format, please use ls8.py examples/file_to_parse format!")
+    sys.exit(1)
+
+cpu.load(sys.argv[1])
+cpu.run()
